362476cf053e061101c7c9979d3acc5aada0b513
evanRez <ereznice@gmail.com>
2022-10-23 11:20:38 -0400

    stage and commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿namespace TeleprompterConsole;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        await RunTelePrompter();
+    }
+    private static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+        using (var reader = File.OpenText(file))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                var words = line.Split(' ');
+                var lineLength = 0;
+                foreach (var word in words)
+                {
+                    yield return word + " ";
+                    lineLength = word.Length + 1;
+                    if (lineLength > 70)
+                    {
+                        yield return Environment.NewLine;
+                        lineLength = 0;
+                    }
+                }
+                yield return Environment.NewLine;
+            }
+        }
+    }
+    private static async Task ShowTeleprompter(TelePrompterConfig config)
+    {
+        var words = ReadFrom("sampleQuotes.txt");
+        foreach (var word in words)
+        {
+            Console.Write(word);
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                await Task.Delay(config.DelayInMilliseconds);
+            }
+        }
+        config.SetDone();
+    }
+    private static async Task GetInput(TelePrompterConfig config) 
+    {
+        Action work = () => 
+        {
+            do 
+            {
+                var key = Console.ReadKey(true);
+                if (key.KeyChar == '>')
+                {
+                    config.UpdateDelay(-10);
+                }
+                else if (key.KeyChar == '<')
+                {
+                    config.UpdateDelay(+10);
+                }
+                else if (key.KeyChar == 'X' || key.KeyChar == 'x')
+                {
+                    config.SetDone();
+                }
+            } while (!config.Done);
+        };
+        await Task.Run(work);
+    }
+    private static async Task RunTelePrompter()
+    {
+        var config = new TelePrompterConfig();
+        var displayTask = ShowTeleprompter(config);
+
+        var speedTask = GetInput(config);
+        await Task.WhenAny(displayTask, speedTask);
+    }
+}
